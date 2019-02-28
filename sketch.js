c5edc880220312bcfe29c4e80fd994b159bb49f5
jDeetz <jdeetz@randolphcollege.edu>
2019-02-28 18:50:16 -0500

    Added comment about resource blocking, added animation to test speed

--- sketch.js
+++ sketch.js
@@ -9,6 +9,19 @@ function setup() {
     canvas.parent('graph'); //Moves graph from end-of-body to an in-page node for inline display
 }
 
+//This is the draw loop, it's called recurrently at ~30fps
+var circleX = 200;
+var circleXMovement = -1;
+var circleY = 190;
+var circleYMovement = -1;
 function draw() {
-  
+  circleX += circleXMovement;
+  circleY += circleYMovement+0.3;
+  if(circleX > 400 || circleX < 0) {
+    circleXMovement = -circleXMovement;
+  }
+  if(circleY > 400 || circleY < 0) {
+    circleYMovement = -circleYMovement;
+  }
+  ellipse(circleX,circleY,20,20); //adding a bouncing ellipse to test rendering effect on page load
 }
