9b14d28882f2613a31192a81b51e000465a2e166
jDeetz <jdeetz@randolphcollege.edu>
2019-03-01 14:07:52 -0500

    Fixing awful soft-tabs

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ This is the code for the graph rendering functions, leveraging p5.js
 
 new p5(); //Create p5 global reference so that math functions like random() are accessible outside of setup()
 function setup() {
-    var canvas = createCanvas(400,400); //Canvas instantiation - dimensions of 400x400 pixels used for the sake of testing on Khan
-    canvas.parent('graph'); //Moves graph from end-of-body to an in-page node for inline display
+	var canvas = createCanvas(400,400); //Canvas instantiation - dimensions of 400x400 pixels used for the sake of testing on Khan
+	canvas.parent('graph'); //Moves graph from end-of-body to an in-page node for inline display
 }
 
 var stepData = [];
