f2ac3288ed5f487b607f77aa7f60ce3583a7dc21
jDeetz <jdeetz@randolphcollege.edu>
2019-03-01 15:44:10 -0500

    Added data parameter to graph type constructor functions

--- sketch.js
+++ sketch.js
@@ -6,38 +6,38 @@ This is the code for the graph rendering functions, leveraging p5.js
 new p5(); //Create p5 global reference so that math functions like random() are accessible outside of setup()
 function setup() {
 	var canvas = createCanvas(400,400); //Canvas instantiation - dimensions of 400x400 pixels used for the sake of testing on Khan
-	canvas.parent('graph'); //Moves graph from end-of-body to an in-page node for inline display
+	canvas.parent('content'); //Moves graph from end-of-body to an in-page node for inline display
 }
 
 var stepData = [];
 var activityData = [];
 var lifeSpaceData = [];
 
-var Heatmap = function() {
+var Heatmap = function(data) {
 
 }
 
-Heatmap.prototype.render(range) {
+Heatmap.prototype.render = function(range) {
 
 }
 
-var Stepgraph = function() {
+var Stepgraph = function(data) {
 
 }
 
-Stepgraph.prototype.render(range) {
+Stepgraph.prototype.render = function(range) {
 
 }
 
-var LifespaceDelta = function() {
+var LifespaceDelta = function(data) {
 
 }
 
-LifespaceDelta.prototype.render(range) {
+LifespaceDelta.prototype.render = function(range) {
 
 }
 
 //This is the draw loop, it's called recurrently at ~30fps
 function draw() {
-
+	
 }
