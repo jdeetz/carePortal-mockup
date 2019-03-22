c08be8bc94c948fece17b09a80b61382806db527
jDeetz <jdeetz@randolphcollege.edu>
2019-03-22 00:02:57 -0400

    Added function for calculating distance from GPS coords regardles of lat

--- sketch.js
+++ sketch.js
@@ -66,6 +66,29 @@ for(var i = 0;i < 2000;i++) {
 	gpsCoords[i][1] = random(70.1,71.9);
 }
 
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
+function fibonacci(num) {  if (num <= 1) return 1;  return fibonacci(num - 1) + fibonacci(num - 2);}
+
 
 
 //These are data structure helper functions for the different types of data;
