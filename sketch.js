77444a46852ad210e4f6cccd5b5eda0d81a3c202
jDeetz <jdeetz@randolphcollege.edu>
2019-03-22 01:13:08 -0400

    Added data argument to data helper class constructor functions

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ This is the code for the graph rendering functions, leveraging p5.js
 
 new p5(); //Create p5 global reference so that math functions like random() are accessible outside of setup()
 function setup() {
-	var canvas = createCanvas(400,400); //Canvas instantiation - dimensions of 400x400 pixels used for the sake of testing on Khan
+	var canvas = createCanvas(400,400); //Canvas instantiation - dimensions of 400x400 pixels used for the sake of testing
 	canvas.parent('content'); //Moves graph from end-of-body to an in-page node for inline display
 }
 
@@ -93,17 +93,17 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 
 //This is the helper function for step data, and will give the data as an array or array of arrays in a custom object type that
 //allows for easier iteration through the data
-var StepData = function() {
+var StepData = function(data) {
 
 };
 
 //Same deal for activity data
-var ActivityData = function() {
+var ActivityData = function(data) {
 
 };
 
 //And for lifeSpace(tm) data
-var LifeSpaceData = function() {
+var LifeSpaceData = function(data) {
 
 };
 
