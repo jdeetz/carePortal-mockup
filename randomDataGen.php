66d6a1043d9a65e5e01705e2e4e1b481bcd4fd5b
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 04:14:43 -0400

    Okay, seeing if it just hates concatenation

--- randomDataGen.php
+++ randomDataGen.php
@@ -12,7 +12,7 @@ $inputData = random_int(0,6000);
 $randmon = "0" . rand(3,4);
 $timeData = random_int(2018,2019) . "-" . $randmon . "-" . random_int(10,30) . " " . random_int(10,23) . ":" . random_int(10,59) . ":" . random_int(10,59);
 
-$sql = "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, $timeData, $inputData)";
+$sql = "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, current_timestamp(), $inputData)";
 $result = $conn->query($sql);
 //
 echo $result;
