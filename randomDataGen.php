d21f5ebef3ab361646dc63bba4097b3176a8fd8f
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 04:13:56 -0400

    trying refactor from rand() to random_int()

--- randomDataGen.php
+++ randomDataGen.php
@@ -8,9 +8,9 @@ if ($conn->connect_error) {
     echo("Connection failed: " . $conn->connect_error);
 }
 
-$inputData = rand(0,6000);
+$inputData = random_int(0,6000);
 $randmon = "0" . rand(3,4);
-$timeData = rand(2018,2019) . "-" . $randmon . "-" . rand(10,30) . " " . rand(10,23) . ":" . rand(10,59) . ":" . rand(10,59);
+$timeData = random_int(2018,2019) . "-" . $randmon . "-" . random_int(10,30) . " " . random_int(10,23) . ":" . random_int(10,59) . ":" . random_int(10,59);
 
 $sql = "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, $timeData, $inputData)";
 $result = $conn->query($sql);
