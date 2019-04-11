204adb0e7fd9ca15590b26e77745b3d609c21b6c
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 02:50:46 -0400

    Created test data generation and insert scripts

---
+++ demoDataGenerator.js
@@ -0,0 +1,30 @@
+//This function generates a bunch of realistic-seeming demo data and sends it to the database insert function
+function ajax_post(url, postData, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(postData);
+}
+
+function hopeThisWorks() {
+	ajax_post("demoDataSubmitter.php","input=54321",function(response) {
+		console.log(response);
+	});
+};
+
+
+//MySQL formatted timestamp date generator:
+//new Date().toISOString().slice(0, 19).replace('T', ' ');
---
+++ demoDataSubmitter.php
@@ -0,0 +1,18 @@
+<?php
+require "creden.php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    echo("Connection failed: " . $conn->connect_error);
+}
+
+$inputData = $_POST['input'];
+
+$sql = "INSERT INTO `DEMO_STEP` (`id`, `times`, `steps`) VALUES (NULL, current_timestamp(), `$inputData`)";
+$result = $conn->query($sql);
+//
+echo $result;
+$conn->close();
+?>
