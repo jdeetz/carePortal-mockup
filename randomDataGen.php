daf70e60afc517ef09b6aa2e77b6eeac4ea4a2c1
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 13:57:52 -0400

    Testing PHP for() loop

--- randomDataGen.php
+++ randomDataGen.php
@@ -8,6 +8,17 @@ if ($conn->connect_error) {
     echo("Connection failed: " . $conn->connect_error);
 }
 
+for($i = 0; $i < 25;$i++) {
+	$inputData = random_int(0,6000);
+	$randYear = random_int(2018,2019);
+	$randDay = random_int(10,30);
+	$randMon = "0" . random_int(3,4);
+	$randHour = random_int(10,23);
+	$randMin = random_int(10,59);
+	$randSec = random_int(10,59);
+	$timeData = $randYear . "-" . $randMon . "-" . $randDay . " " . $randHour . ":" . $randMin . ":" . $randSec;
+	$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData);";
+}
 $inputData = random_int(0,6000);
 $randYear = random_int(2018,2019);
 $randDay = random_int(10,30);
@@ -17,8 +28,8 @@ $randMin = random_int(10,59);
 $randSec = random_int(10,59);
 $timeData = $randYear . "-" . $randMon . "-" . $randDay . " " . $randHour . ":" . $randMin . ":" . $randSec;
 
-$sql = "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData)";
-$result = $conn->query($sql);
+$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData)";
+$result = $conn->multi_query($sql);
 //
 echo $result;
 echo "It seems to have worked I guess";
