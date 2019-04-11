20721c7c6be11c56ddce0dd794ef64557ef590e7
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 15:07:44 -0400

    Created full blown data gen functions, unlikely to work first time thru

--- randomDataGen.php
+++ randomDataGen.php
@@ -8,7 +8,50 @@ if ($conn->connect_error) {
     echo("Connection failed: " . $conn->connect_error);
 }
 
-for($i = 0; $i < 50000;$i++) {
+$homeLat = 37.433595;
+$homeLon = -79.158150;
+$workLat = 37.436131;
+$workLon = -79.170871;
+$sevenLat = 37.434644;
+$sevenLon = -79.168029;
+$krogLat = 37.442378;
+$krogLon = -79.204132;
+$kelleyLat = 37.428052;
+$kelleyLon = -79.172024;
+
+function randFloat($min,$max) {
+	$min = $min * 1000000;
+	$max = $max * 1000000;
+	return mt_rand($min,$max)/1000000;
+}
+
+function generateRandTimestamp() {
+	$randYear = random_int(2018,2019);
+	$randDay = random_int(10,30);
+	$randMon = "0" . random_int(2,4);
+	$randHour = random_int(10,23);
+	$randMin = random_int(10,59);
+	$randSec = random_int(10,59);
+	$timeData = $randYear . "-" . $randMon . "-" . $randDay . " " . $randHour . ":" . $randMin . ":" . $randSec;
+	return $timeData;
+}
+
+function generateRandData($numTimes,$forLat,$forLon) {
+	for($i = 0; $i < $numTimes;$i++) {
+		$minLat = $forLat - ($forLat / 1000);
+		$maxLat = $forLat + ($forLat / 1000);
+		$inputLat = randFloat($minLat,$maxLat);
+		$minLon = $forLon - ($forLon / 1000);
+		$maxLon = $forLon + ($forLon / 1000);
+		$inputLon = randFloat($minLon,$maxLon);
+		$timeData = generateRandTimestamp();
+		$inputSteps = random_int(0,100);
+		$sql .= "INSERT INTO DEMO_GPS (`id`, `times`, `lat`, `long`) VALUES (NULL, '$timeData', '$inputLat', '$inputLon');";
+		$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputSteps)";
+	}
+}
+
+function generateLast() {
 	$inputData = random_int(0,6000);
 	$randYear = random_int(2018,2019);
 	$randDay = random_int(10,30);
@@ -17,21 +60,25 @@ for($i = 0; $i < 50000;$i++) {
 	$randMin = random_int(10,59);
 	$randSec = random_int(10,59);
 	$timeData = $randYear . "-" . $randMon . "-" . $randDay . " " . $randHour . ":" . $randMin . ":" . $randSec;
-	$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData);";
+	$sql .= "INSERT INTO DEMO_GPS (`id`, `times`, `lat`, `long`) VALUES (NULL, '$timeData', '37.433595', '-79.158150')";
+	$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData)";
+	$result = $conn->multi_query($sql);
 }
-$inputData = random_int(0,6000);
-$randYear = random_int(2018,2019);
-$randDay = random_int(10,30);
-$randMon = "0" . random_int(3,4);
-$randHour = random_int(10,23);
-$randMin = random_int(10,59);
-$randSec = random_int(10,59);
-$timeData = $randYear . "-" . $randMon . "-" . $randDay . " " . $randHour . ":" . $randMin . ":" . $randSec;
 
-$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData)";
-$result = $conn->multi_query($sql);
+function createAndSubData() {
+	global $homeLat, $homeLon, $workLat, $workLon, $sevenLat, $sevenLon, $krogLat, $krogLon, $kelleyLat, $kelleyLon;
+	generateRandData(500,$homeLat,$homeLon);
+	generateRandData(500,$workLat,$workLon);
+	generateRandData(500,$sevenLat,$sevenLon);
+	generateRandData(500,$krogLat,$krogLon);
+	generateRandData(500,$kelleyLat,$kelleyLon);
+	generateLast();
+}
+
+createAndSubData();
+
 //
 echo $result;
-echo "It seems to have worked I guess";
+echo "Okay cool so it submitted probably";
 $conn->close();
 ?>
