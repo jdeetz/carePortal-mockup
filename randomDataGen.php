f3eb00feb3002fc2c0302e2b214790ee8e16e960
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 21:52:05 -0400

    Removed test code, added sarcastic, snarky comments

--- randomDataGen.php
+++ randomDataGen.php
@@ -1,14 +1,17 @@
 <?php
 
-
+#This function generates a random float within a range, since all of PHP's random number functions give you an int for some reason
 function randFloat($min,$max) {
+	#multiply both numbers by a million so that you can pass in floats for your range - in this case, GPS coordinates
 	$min = $min * 1000000;
 	$max = $max * 1000000;
+	#We divide the result by a million because that's how math works
 	return mt_rand($min,$max)/1000000;
 }
 
 
 function generateRandTimestamp() {
+	#I split this into individual functions because PHP hates inline execution. PHP is not a good language.
 	$randYear = random_int(2018,2019);
 	$randDay = random_int(10,30);
 	$randMon = "0" . random_int(2,4);
@@ -21,6 +24,8 @@ function generateRandTimestamp() {
 
 
 function generateRandData($numTimes,$forLat,$forLon) {
+	#Moved the database connection code and stuff into this function because scoping
+	#is apparently not PHP's strong suit. Global variables are not global, which is great because ...
 	require "creden.php";
 
 	// Create connection
@@ -30,12 +35,14 @@ function generateRandData($numTimes,$forLat,$forLon) {
 	    echo("Connection failed: " . $conn->connect_error);
 	}
 
+	#This part is pretty much necessary because when we instantiate the SQL variable, even with an empty string, and then
+	#later use it AS a complex string, PHP gets confused and hurts itself in its confusion
 	$inputData = random_int(0,6000);
 	$timeData = generateRandTimestamp();
 	$sql = "INSERT INTO DEMO_GPS (`id`, `times`, `lat`, `lon`) VALUES (NULL, '$timeData', '37.433595', '-79.158150');";
 	$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData);";
 
-
+	#This is the part that actually generates a bunch of random data, equivalent to how much we asked it to up above
 	for($i = 0; $i < $numTimes;$i++) {
 		$minLat = $forLat - ($forLat / 1000);
 		$maxLat = $forLat + ($forLat / 1000);
@@ -47,23 +54,25 @@ function generateRandData($numTimes,$forLat,$forLon) {
 		$inputSteps = random_int(0,100);
 		$sql .= "INSERT INTO DEMO_GPS (`id`, `times`, `lat`, `lon`) VALUES (NULL, '$timeData', '$inputLat', '$inputLon');";
 		$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputSteps);";
-		echo "have run " . $i . " times";
-		$roundNum = 2 + $i;
 	}
 
+	#And since PHP also has trouble with conditionals (see the note about inline execution above) we also have to
+	#do the last concatenation separately
 	$inputData = random_int(0,6000);
 	$timeData = generateRandTimestamp();
 	$sql .= "INSERT INTO DEMO_GPS (`id`, `times`, `lat`, `lon`) VALUES (NULL, '$timeData', '37.433595', '-79.158150');";
 	$sql .= "INSERT INTO DEMO_STEP (`id`, `times`, `steps`) VALUES (NULL, '$timeData', $inputData)";
 
-	$totalRounds = ($numTimes * 2) + 4;
-
+	#at long last, we execute our SQL query
 	$result = $conn->multi_query($sql);
 	$conn->close();
 }
 
 
+#This function calls the other function. Our function chain is exactly 2 long, because
+#PHP's motto is "More than 2 long is too long". At least I assume that's the motto, based on the behavior observed
 function createAndSubData() {
+	#These variables are used for seeding the data with a variety of normal places, like a house, grocery store, gas station etc.
 	$homeLat = 37.433595;
 	$homeLon = -79.158150;
 	$workLat = 37.436131;
@@ -75,6 +84,7 @@ function createAndSubData() {
 	$kelleyLat = 37.428052;
 	$kelleyLon = -79.172024;
 
+	#This is "where the magic happens" and by magic, I mean PHP working in a predictable manner
 	generateRandData(721,$homeLat,$homeLon);
 	generateRandData(1283,$workLat,$workLon);
 	generateRandData(79,$sevenLat,$sevenLon);
