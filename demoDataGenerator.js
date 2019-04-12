fb97e9fb3dffa230ed47b47efbb5e6cfd932f3eb
jDeetz <jdeetz@randolphcollege.edu>
2019-04-11 21:55:26 -0400

    Removed redundant demo data generator files, labelled submission func()

--- demoDataGenerator.js
+++ demoDataGenerator.js
@@ -1,4 +1,4 @@
-//This function generates a bunch of realistic-seeming demo data and sends it to the database insert function
+//This function is for securely submitting data to the database via POST
 function ajax_post(url, postData, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -22,18 +22,12 @@ function ajax_post(url, postData, callback) {
 function hopeThisWorks() {
 	var fred1 = new Date().toISOString().slice(0, 10);
 	var fred2 = new Date().toISOString().slice(11, 19);
-	var sendData = "input=888888&time1=" + fred1 + "&time2=" + fred2;
+	//TODO - make this line send actual data
+	var sendData = "input=abcdef";
 	console.log(sendData);
 	ajax_post("demoDataSubmitter.php",sendData,function(response) {
 		document.write(response);
 	});
-};
-
-function testDate() {
-	var fred1 = new Date().toISOString().slice(0, 9);
-	var fred2 = new Date().toISOString().slice(11, 19);
-	console.log(fred1);
-	console.log(fred2);
 };
 
 
